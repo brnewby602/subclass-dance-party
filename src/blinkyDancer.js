2f936bed5fd07dcde74650621d2ff74e8ceb5c50
brnewby602 <brnewby602@gmail.com>
2016-03-08 12:14:37 -0800

    implement move()

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -8,5 +8,28 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
+
+  if (!this._lineUpFlag) {
+    this.move();
+  }
   // this.$node.toggle();
+};
+
+makeBlinkyDancer.prototype.move = function() {
+  var bottomBound = window.windowHeight - 60;
+  var rightBound = window.windowWidth - 60;
+  var topBound = 0;
+  var leftBound = 0;
+
+  var rand = (Math.random() - 0.5) * 100;
+
+  if ((rand + this._top >= topBound) && (rand + this._top <= bottomBound)) {
+    this._top += rand;
+  } 
+
+  if ((rand + this._left >= leftBound) && (rand + this._left <= bottomBound)) {
+    this._left += rand;
+  }
+
+  this.setPosition(this._top, this._left);
 };
