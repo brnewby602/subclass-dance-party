919c8ef1b0f00be24dc359f0a5cf8c120c5bf1ce
brnewby602 <brnewby602@gmail.com>
2016-03-07 20:54:13 -0800

    implement getWidth method in blinkyDancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -8,5 +8,5 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-  this.$node.toggle();
+  // this.$node.toggle();
 };
