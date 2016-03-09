dd791afdb221783aa79d8ccdf259bb9c3a3551d1
brnewby602 <brnewby602@gmail.com>
2016-03-08 19:32:23 -0800

    Add overridden dance function

--- src/bouncingDancer.js
+++ src/bouncingDancer.js
@@ -9,16 +9,14 @@ makeBouncingDancer.prototype.constructor = makeBouncingDancer;
 makeBouncingDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
   // this.$node.toggle();
-  
-  // console.dir(this.$node);
-  // console.log("Top = " + this._top);
-  // console.log("Left = " + this._left);
-  // console.log(this.$node[0].outerHTML);
-  // this.$node.toggle();
-    // this.$node.toggle( "bounce", { times: 10 }, "slow" );
-    // this.$node.slideToggle();
-  // console.log("AFTER TOGGLE: " + this.$node[0].outerHTML);
-  // this.$node.bounce();
-  // this.$node.toggle();
-  //this.$node.toggle("bounce", { times: 1 }, "slow");
+};
+
+makeBouncingDancer.prototype.dance = function() {
+  var styleSettings = {
+    top: this._top,
+    left: this._left //, 
+  };
+
+  this.$node.toggleClass('bounce');    //need to create .bounce class
+
 };
